31f77b58a5d1df5304728e1ef932e66932e2b1d0
Devildice666 <63431193+ikhwanketor@users.noreply.github.com>
2026-02-28 09:23:56 +0700

    Delete src directory

--- src/components/ProgressBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProgressBar = ({ progress }) => {
-    return (
-        <div className="progress-bar" style={{width: `${progress}%`}}></div>
-    );
-};
-
-export default ProgressBar;
